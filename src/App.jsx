2fbb402cdd5bcd029451d423cad34be207799893
kosterseb <sebkos.code@gmail.com>
2025-11-11 10:59:09 +0100

    added the rest, gonna run tests

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-import { useState } from 'react'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SplashScreen from './components/SplashScreen';
+import Navigation from './components/Navigation';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import Gallery from './pages/Gallery';
+import Explore from './pages/Explore';
+import Guestbook from './pages/Guestbook';
+import './styles/main.scss';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [showSplash, setShowSplash] = useState(true);
+
+  const handleSplashComplete = () => {
+    setShowSplash(false);
+  };
 
   return (
     <>
+      {showSplash && <SplashScreen onComplete={handleSplashComplete} />}
+      {!showSplash && (
+        <Router basename="/cecilia-monte-cantini">
+          <Navigation />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/gallery" element={<Gallery />} />
+            <Route path="/explore" element={<Explore />} />
+            <Route path="/guestbook" element={<Guestbook />} />
+          </Routes>
+          <Footer />
+        </Router>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
