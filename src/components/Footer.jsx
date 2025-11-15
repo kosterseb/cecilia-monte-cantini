f83bd15a605d346ddb9c6e5822349ee8a1d94d1d
kosterseb <sebkos.code@gmail.com>
2025-11-15 22:08:37 +0100

    Edit: Made some changes to the footer and contact.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,14 +9,16 @@ const Footer = () => {
             <h3>Donatello</h3>
             <p>Your home away from home in the heart of Tuscany</p>
           </div>
-          
+
           <div className="footer-section">
             <h4>Contact</h4>
-            <p>Viale Giacomo Puccini n.16</p>
-            <p>51016 Montecatini Terme</p>
-            <p>Provincia: Pistoia, Italy</p>
+            <h5>For info:</h5>
+            <p>casa@donatello.dk</p>
+            <br />
+            <h5>For reservations:</h5>
+            <p>hotel@donatello.dk</p>
           </div>
-          
+
           <div className="footer-section">
             <h4>Location</h4>
             <p>30 minutes from Florence</p>
@@ -24,7 +26,7 @@ const Footer = () => {
             <p>30 minutes from Pisa Airport</p>
           </div>
         </div>
-        
+
         {/* Credits */}
         <div className="footer-bottom">
           <p>&copy; {new Date().getFullYear()} Hotel Donatello. Montecatini Terme, Tuscany.</p>
